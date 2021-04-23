9b725b1c8384bef303df337daf5982ea1a6ed735
diyamanandhar <dia.mdhr01@gmail.com>
2021-04-23 18:23:48 +0545

    m

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,46 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+const NavbarWrapper = styled.nav`
+  margin: 0;
+  padding: 10px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const NavListWrapper = styled.ul`
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+
+  li {
+    list-style: none;
+
+    a {
+      text-decoration: none;
+    }
+  }
+`;
 
 const Navbar = () => {
-	return (
-		<nav>
-			<div className="logo">KG</div>
-			<div className="links">
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/about">About</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <NavbarWrapper>
+      <div className="logo">KG</div>
+      <div className="links">
+        <ul>
+          <NavListWrapper>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </NavListWrapper>
+        </ul>
+      </div>
+    </NavbarWrapper>
+  );
 };
 
 export default Navbar;
